c1a331ee787fc7a84f30143803ab32ff70e1bbc8
Nick Gish <nicholas.gish@gmail.com>
2018-09-06 13:57:25 -0400

    revealing something like a cake, but different in a few crucial ways

---
+++ js/gen.js
@@ -0,0 +1,32 @@
+let oneArray = ["r", "d", { r: "r" }];
+
+const newOne = [".....", "shhhh", ...oneArray];
+
+const newOneToo = [
+  ...oneArray, "you crack me up",
+  { question: "you want a new slice of pecan pie?" }
+];
+
+const thirdOne = () => {
+  const qPlace = document.querySelector('.q');
+  const pieSlice = document.querySelector('.pieHolder');
+    setTimeout(() => {
+      qPlace.innerHTML = newOneToo.slice(-1)[0].question;
+      qPlace.style.cssText = "opacity: 1";
+      pieSlice.style.cssText = "opacity: 1; transform: translate3d(0, 0, 0)";
+    }, 1000);
+};
+
+function* generate(gen) {
+  const firstAdd = yield newOne;
+  const secondAdd = yield newOneToo;
+  const thirdAdd = yield thirdOne();
+}
+
+const numOfIterate = 4;
+let gens = generate(numOfIterate);
+
+console.log(gens.next());
+console.log(gens.next());
+gens.next();
+console.log(gens.next(), newOneToo.slice(-1)[0]);
